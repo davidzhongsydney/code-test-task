b787cb621577d9723b713923a1e791d0e0edbfdb
davidzhongsydney <david.zhong.uts@gmail.com>
2023-03-15 21:00:12 +1100

    Add config error_reason task proto Makefile

--- cmd/task-server/main.go
+++ cmd/task-server/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/go-kratos/kratos/v2/config"
+	"github.com/go-kratos/kratos/v2/config/file"
+	conf "qantas.com/task/internal/conf"
+	model "qantas.com/task/model"
+)
+
+var path = "../../configs/config.yaml"
 
 func main() {
-	fmt.Println("test")
+	var task model.Task
+	fmt.Println(task.Name)
+
+	c := config.New(
+		config.WithSource( // 初始化配置源
+			file.NewSource(path),
+		),
+	)
+	// load config source
+	if err := c.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	var bc conf.Bootstrap
+
+	if err := c.Scan(&bc); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(bc.Server.Http.Timeout)
+	// Get the corresponding value
+	// name, err := c.Value("server.http.timeout").String()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(name)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module qantas.com/task
 
 go 1.19
+
+require github.com/go-kratos/kratos/v2 v2.6.1
+
+require (
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	google.golang.org/protobuf v1.29.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
+)
